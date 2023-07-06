feebb99626821068ca731f825373492b3858225b
Adam O'Neil <adamosoftware@gmail.com>
2023-07-06 10:35:11 -0400

    Add project files.

---
+++ RoslynScraper/Models/Input.cs
@@ -0,0 +1,14 @@
+﻿namespace RoslynScraper.Models;
+
+public class Input
+{
+	/// <summary>
+	/// VS .sln file to inspect
+	/// </summary>
+	public string SolutionFile { get; set; } = default!;
+
+	/// <summary>
+	/// what interfaces do I want to inspect?
+	/// </summary>
+	public IEnumerable<string> InspectInterfaces { get; set; } = Enumerable.Empty<string>();	
+}
---
+++ RoslynScraper/Models/Output.cs
@@ -0,0 +1,27 @@
+﻿namespace RoslynScraper.Models;
+
+public class Output
+{
+	public IEnumerable<Item> Items { get; set; } = Enumerable.Empty<Item>();
+
+	public class Item
+	{
+		/// <summary>
+		/// class namespace
+		/// </summary>
+		public string Namespace { get; set; } = default!;
+		/// <summary>
+		/// containing class
+		/// </summary>
+		public string ClassName { get; set; } = default!;
+		/// <summary>
+		/// method or property name (or event or other member type, honestly)
+		/// </summary>
+		public string MemberName { get; set; } = default!;
+		/// <summary>
+		/// method or property body
+		/// </summary>
+		public string Body { get; set; } = default!;
+	}
+}
+
